bcfa242c28b96a856dd58a2012105cdc9ac1f5b1
shunpin tseng <shunpin@gmail.com>
2020-09-20 23:30:30 -0700

    code update

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-console.log("hello world")
+//jquery HTML elementsearch button for the city name input
 
 
 
-var cityNmae = $(".form-inline")
 
-api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+
+$("button").on("click", function (event) {
+    event.preventDefault();
+    var cityName = $("input").val()
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=8ce642e2d21bbeaa43533bd47e431320";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+
+        var newCityBtn = $('<a>');
+        newCityBtn.addClass("list-group-item");
+        newCityBtn.attr("data-toggle", "list")
+        newCityBtn.text(cityName);
+        $(".list-group").append(newCityBtn);
+
+        var disCityName = $("<h1>");
+        disCityName.text(response.city.name);
+        $(".container").append(disCityName)
+
+        var disDate = $("<h2>");
+        disDate.text(response.list[0].dt_txt);
+        $(".container").append(disDate)
+
+
+    })
+})
+
+
+
